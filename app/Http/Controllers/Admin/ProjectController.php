c0dc6905f79b388678fea890621f5fecba366233
Jonathan-Ultini <jonathanultini@gmail.com>
2024-11-10 09:34:37 +0100

    utilizzo request nel controller project

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Http\Requests\StoreProjectRequest;
+use App\Http\Requests\UpdateProjectRequest;
+
 class ProjectController extends Controller
 {
     /**
@@ -26,9 +29,10 @@ class ProjectController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreProjectRequest $request)
     {
-        //
+        Project::create($request->validated());
+        return redirect()->route('admin.projects.index')->with('success', 'Progetto creato con successo');
     }
 
     /**
@@ -50,9 +54,10 @@ class ProjectController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateProjectRequest $request, Project $project)
     {
-        //
+        $project->update($request->validated());
+        return redirect()->route('admin.projects.index')->with('success', 'Progetto aggiornato con successo');
     }
 
     /**
